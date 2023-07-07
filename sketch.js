d8fd563cef7db3b97e56773042fa251ea1f74e3a
asib27 <asib.rahman1927@gmail.com>
2023-07-07 13:00:34 +0600

    add basic file

---
+++ sketch.js
@@ -0,0 +1,10 @@
+const CANVAS_WIDTH = window.innerWidth;
+const CANVAS_HEIGHT = window.innerHeight;
+
+function setup() {
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+  
+function draw() {
+    background(255, 0, 0);
+}
